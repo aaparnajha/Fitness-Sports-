db91294cc5c11e6171f8b18f3103f6265b35a791
aaparnajha <157946649+aaparnajha@users.noreply.github.com>
2024-10-04 20:58:58 +0530

    Add files via upload

---
+++ Project folder/gfg.js
@@ -0,0 +1,5 @@
+// JavaScript for enhancing user interaction on the home page
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Welcome to the Yoga & Sports for Wellness Page!");
+    // Add more JavaScript functions as needed
+});
